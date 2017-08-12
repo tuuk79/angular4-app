7d48ce0028cf3fb9619fa5391edb45b31e25fcb4
tuuk79 <stevecyoo@gmail.com>
2017-08-11 21:25:00 -0500

    adding sample tests for login service

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -1,3 +1,14 @@
+import { LoginService } from './login.service';
+
 describe('login service', () => {
-	it('should get created', () => expect(true).toBe(true));
+	it('should get created', () => {
+		const loginService = new LoginService();
+		expect(loginService).not.toBe(null);
+	});
+
+	it('should login successfully', () => {
+		const loginService = new LoginService();
+		const response = loginService.getHello();
+		expect(response).toBe('hello');
+	});
 });
